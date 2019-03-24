5bb14a9e0e82e28a916a5ce0c94396614a84b0f5
pengyiduanshiguan <2640821665@qq.com>
2019-03-24 21:13:59 +0800

    0322

---
+++ test.c
@@ -0,0 +1,48 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main() {
+//	int i, j;
+//	for (i = 100; i <= 200; ++i) {
+//		for (j = 2; j < i ; ++j) {
+//			if (i%j == 0) {
+//				break;
+//			}
+//		}
+//		if (j == i) {
+//			printf("%d\n", i);
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main() {
+//	int i, j;
+//	for (i = 1; i <= 9; i++) {
+//		for (j = 1; j < i + 1; j++) {
+//			printf("%d*%d=%d\t", j, i, i*j);
+//		}
+//		printf("\n");
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main() {
+//	int x;
+//	scanf("%d", &x);
+//		if (x % 400 == 0 || x % 4 == 0 && x % 100 != 0)
+//		{
+//			printf("%d年是闰年\n", x);
+//		}
+//		else{
+//			printf("%d年不是闰年\n", x);
+//		}
+//	system("pause");
+//	return 0;
+//}
