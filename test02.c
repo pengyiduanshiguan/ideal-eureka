02a34976656b3b39e4510a56426b16b10ec77a3b
pengyiduanshiguan <2640821665@qq.com>
2019-03-25 23:32:12 +0800

    //求 1+1/(-2)+1/3+....+1/(-100).

---
+++ test02.c
@@ -0,0 +1,18 @@
+//求0到99中遇到9的次数。
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main() {
+//	int i;
+//	int tmp=0;
+//	for (i = 1; i <= 100; ++i) {
+//		if (i % 10 == 9){
+//			tmp += 1;
+//		}
+//		if (i/10 == 9) {
+//			tmp += 1;
+//		}	
+//	}
+//	printf("%d\n", tmp);
+//	system("pause");
+//	return 0;
+//}
