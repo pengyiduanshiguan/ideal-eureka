4444e8c4090c291c9be049900baba7029a5beb60
pengyiduanshiguan <2640821665@qq.com>
2019-03-25 23:29:58 +0800

    将两个数组中的内容进行交换（两个数组一样大）

---
+++ test (2).c
@@ -0,0 +1,38 @@
+//将两个数组中的内容进行交换（两个数组一样大）
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdlib.h>
+//#include <stdio.h>
+//int main() {
+//	//引入第三个数组进行数组个元素的交换
+//	int a[3];
+//	int b[3];
+//	int c[3];
+//	int i, j, k;
+//	printf("请输入数组a[3]:");
+//	for (i = 0; i < 3; ++i) {
+//		scanf("%d", &a[i]);
+//	}
+//	printf("请输入数组b[3]:");
+//	for (j = 0; j < 3; ++j) {
+//		scanf("%d", &b[j]);
+//	}
+//	for (k = 0; k < 3; ++k) {
+//		c[k] = a[k];
+//	}
+//	for (i = 0; i < 3; ++i) {
+//		a[i] = b[i];
+//	}
+//	for (j = 0; j < 3; ++j) {
+//		b[j] = c[j];
+//	}
+//	printf("交换后数组a为:");
+//	for (i = 0; i < 3; ++i) {
+//		printf("%d ", a[i]);
+//	}
+//	printf("交换后数组b为:");	
+//	for (j = 0; j < 3; ++j) {
+//		printf("%d ", b[j]);
+//	}
+//	system("pause");
+//	return 0;
+//}
