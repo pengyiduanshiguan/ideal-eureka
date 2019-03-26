759e6102975c024ef3b2d211e1929a5958c269f8
pengyiduanshiguan <2640821665@qq.com>
2019-03-26 23:34:06 +0800

    * 图案

---
+++ test (3).c
@@ -0,0 +1,22 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main() {
+//	int i,j,n;
+//	int a,b;
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; ++i){
+//		for (j = 1; j <= 2*i-1;++j) {
+//			putchar('*');
+//		}
+//			putchar('\n');
+//	}
+//	for (a = n-1; a >= 1; --a){
+//		for (b = 1; b <= 2 * a - 1;++b) {
+//			putchar('*');
+//		}
+//		putchar('\n');
+//	}
+//	system("pause");
+//	return 0;
+//}
