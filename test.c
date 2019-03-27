c425c74a917c50613d96da42cc9184b2a36ce72a
pengyiduanshiguan <2640821665@qq.com>
2019-03-27 23:35:12 +0800

    猜数字

--- test.c
+++ test.c
@@ -1,48 +1,55 @@
-//#include<stdio.h>
-//#include<stdlib.h>
-//int main() {
-//	int i, j;
-//	for (i = 100; i <= 200; ++i) {
-//		for (j = 2; j < i ; ++j) {
-//			if (i%j == 0) {
-//				break;
-//			}
-//		}
-//		if (j == i) {
-//			printf("%d\n", i);
-//		}
-//	}
-//	system("pause");
-//	return 0;
-//}
-
-//#include<stdio.h>
-//#include<stdlib.h>
-//int main() {
-//	int i, j;
-//	for (i = 1; i <= 9; i++) {
-//		for (j = 1; j < i + 1; j++) {
-//			printf("%d*%d=%d\t", j, i, i*j);
-//		}
-//		printf("\n");
-//	}
-//	system("pause");
-//	return 0;
-//}
-
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<stdio.h>
-//#include<stdlib.h>
-//int main() {
-//	int x;
-//	scanf("%d", &x);
-//		if (x % 400 == 0 || x % 4 == 0 && x % 100 != 0)
-//		{
-//			printf("%d年是闰年\n", x);
-//		}
-//		else{
-//			printf("%d年不是闰年\n", x);
-//		}
-//	system("pause");
-//	return 0;
-//}
+//猜数字
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+int Menu(){
+	printf("===========\n");
+	printf("1.开始游戏\n");
+	printf("0.结束游戏\n");
+	printf("请输入您的选择：\n");
+	printf("===========\n");
+	int choice = 0;
+	scanf("%d", &choice);
+	return choice;
+}
+void Game() {
+	//生成一个随机数
+	//rand表示生成一个随机数，其中rand()%100
+	//表示生成1到100的随机数
+	int num = rand()%100+1;
+	//提示用户输入数字
+	int tmp=0;
+	while (tmp < 10) {
+		printf("请输入您猜的数字：\n");
+		int to_guess ;
+		scanf("%d", &to_guess);
+		//比较用户输入的数字与随机数
+		if (to_guess > num) {
+			printf("您猜大了\n");
+		}
+		else if (to_guess < num) {
+			printf("您猜小了\n");
+		}
+		else {
+			printf("恭喜您猜对了\n");
+			return;
+		}
+		tmp += 1;
+	}
+}
+int main() {
+	while (1) {
+		int choice = Menu();
+		if (choice == 1) {
+			Game();
+		}
+		else if (choice == 0) {
+			printf("再见!\n");
+			break;
+		}else {
+			printf("您的输入有误!\n");
+		}
+	}
+	system("pause");
+	return 0;
+}
